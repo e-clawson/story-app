e43a8befab9128046ccff2f5b9dedaef1e90e9dd
ihollander <ianholla@gmail.com>
2021-06-08 12:02:27 -0400

    Added React Router fallback

---
+++ app/controllers/fallback_controller.rb
@@ -0,0 +1,7 @@
+class FallbackController < ActionController::Base
+
+  def index
+    # React app index page
+    render file: 'public/index.html'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   get "/me", to: "users#show"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
+  # all other routes will be load our React application
+  # this route definition matches:
+  # - *path: all paths not matched by one of the routes defined above
+  # - constraints:
+  #   - !req.xhr?: it's not a XHR (fetch) request
+  #   - req.format.html?: it's a request for a HTML document
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
