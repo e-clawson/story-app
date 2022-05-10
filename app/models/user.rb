f4e045eb1acbe140adc764998abf730acb23eec9
Elizabeth Clawson <emclawson1@gmail.com>
2022-05-10 18:45:31 -0500

    add unique email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     has_secure_password
 
     # validations
-    validates :username, presence: true, length: {in: 4..25}
+    validates :username, uniqueness :true, presence: true, length: {in: 4..25}
     validates :email, presence: true, uniqueness: true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
     validates :age, numericality: {only_integer: true, greater_than: 15}
     validates :password, length: {in: 6..25}
