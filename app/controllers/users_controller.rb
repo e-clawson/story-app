309324448b8bac359a6e375a4f9c2b1f68f0dcb5
Elizabeth Clawson <emclawson1@gmail.com>
2022-05-10 20:14:05 -0500

    add routes to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < ApplicationController
-  
+  def create
+    user = User.create(user_params)
+    session[:user_id] = user.id
+    render json: user 
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :email, :password)
+  end
+
+
 end
