a8c19375222a59773d98b4a09480e300523d2112
Elizabeth Clawson <emclawson1@gmail.com>
2022-05-10 20:19:20 -0500

    add show route to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
   def create
-    user = User.create(user_params)
+    user = User.create!(user_params)
     session[:user_id] = user.id
     render json: user 
   end
+
+  def show 
+    return render json: {error: "Not Authorized"}, status: :unauthorized unless session.include?(:user_id) 
+    user ||= User.find_by_id(session[:user_id])
+    render json: user, status: :ok
+  end
+
 
   private
 
