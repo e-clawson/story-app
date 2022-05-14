b47d024e2455d2de51f29f285d720564d7331834
Elizabeth Clawson <emclawson1@gmail.com>
2022-05-14 16:37:25 -0500

    add home page info

---
+++ client/src/components/About.js
@@ -0,0 +1,20 @@
+
+const About = () => {
+    return (
+      <div>
+          <h1>About</h1>
+          <h2>Mission</h2> 
+          <p>The goal of the double bass database is to provide bassists 
+          with a one-stop shop to look up music related to the double bass.
+          This app will contain a databse including a list of solo (unaccompanied 
+          and accompanied), duet, trio, and chamber music for the bass, along with the 
+          composer name and length of piece. Users can also favorite pieces and create 
+          their own list of double bass favorites, and add to the overall list with new pieces
+          if they see that a piece they know is missing. </p>
+       </div>
+  
+    )
+  }
+  
+  
+  export default About;
---
+++ client/src/components/Home.js
@@ -0,0 +1,13 @@
+import MusicContainer from "../containers/MusicContainer"
+
+const Home = () => {
+  return (
+    <div> 
+    <h1>AnthologyApp!</h1>
+    <h2>Share your short story ideas with the world!</h2>
+    <PromptContainer/>
+    </div>
+  )
+}
+
+export default Home
