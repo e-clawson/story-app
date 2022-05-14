0b08e94e464d7e5ebf1766782a49f31c8f4dd6ca
Elizabeth Clawson <emclawson1@gmail.com>
2022-05-14 17:38:53 -0500

    create my own Login form page

---
+++ client/src/components/Login.js
@@ -0,0 +1,46 @@
+import {useState, useContext} from 'react'
+import { useHistory } from 'react-router-dom'
+import { UserContext } from "../../context/user.js"
+import { MessageContext } from '../../context/message.js'
+
+const Login = () => {
+    const {setUser,login} = useContext(UserContext);
+    const {setMessage} = useContext(MessageContext); 
+
+    const [userObj, setUserObj] = useState({
+        email: "", 
+        password: ""
+    });
+
+    const history = useHistory()
+
+    const handleChange = ({target: {name, value}}) => {
+        setUserObj({
+            ...userObj,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if  ([userObj.email, userObj.password].some(val => val.trim() === "")){
+            alert ("Please provide an email and password")
+        }
+        login(userObj)
+        
+    }
+  return (
+    <>
+        <h3>Login</h3>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Email</label>
+            <input onChange={handleChange} type="text" name="email" value={userObj.email}/><br />
+            <label htmlFor="password">Password</label>
+            <input onChange={handleChange} type="password" name="password" value={userObj.password}/><br />
+            <input type="submit" value="login" />
+        </form>
+    </>
+  )
+}
+
+export default Login;
