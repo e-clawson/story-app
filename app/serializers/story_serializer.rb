31e695fb28b0ded810daf13d937b36ad8ebded9d
Elizabeth Clawson <emclawson1@gmail.com>
2022-05-23 14:02:43 -0500

    update story serializer

--- app/serializers/story_serializer.rb
+++ app/serializers/story_serializer.rb
@@ -1,4 +1,6 @@
 class StorySerializer #< ActiveModel::Serializer
     include JSONAPI::Serializer
-    attributes :story_title, :story_body, :id, 
+    attributes :story_title, :story_body, :id, :prompt_id, :user_id
+    belongs_to :prompt, serializer: PromptSerializer
+    
 end
