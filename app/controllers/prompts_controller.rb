a0cd847334294939efdbc39cbd1252f1eb0782ad
Elizabeth Clawson <emclawson1@gmail.com>
2022-05-23 11:28:39 -0500

    update prompts_controller

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -2,7 +2,7 @@ class PromptsController < ApplicationController
     skip_before_action :authorized!, only: [:index]
     before_action :find_prompt, only: [:show, :update, :destroy]
 
-    def index #get "/posts"
+    def index #get "/prompts"
         render json: PromptSerializer.new(Prompt.preload(:stories)).serializable_hash
     end
 
@@ -11,20 +11,20 @@ class PromptsController < ApplicationController
     # end
 
     def most_comments
-        render json: Post.most_comments
+        render json: Prompt.most_comments
     end
 
-    def show #get "/posts/:id"
-        render json: serialized_post
+    def show #get "/propmts/:id"
+        render json: serialized_prompt
     end
 
     def create #post "/prompts" "users/17/prompts" "users/99/prompts"
         @prompt = current_user.prompts.create!(prompt_params)
-        # if @prompt.id
+        if @prompt.id
         render json: serialized_prompt, status: 201
-        # else
-            # render json: {error: @prompt.errors.full_messages.to_sentence}
-        # end
+        else
+            render json: {error: @prompt.errors.full_messages.to_sentence}
+        end
     end
 
     def update #patch "/prompts/:id"
@@ -39,10 +39,10 @@ class PromptsController < ApplicationController
         # end
     end
 
-    # def destroy #delete "/posts/:id"
-    #     if current_user.posts.include?(@post)
-    #         if @post&.destroy
-    #             render json: {message: "Successfully destroyed post!"}
+    # def destroy #delete "/prompts/:id"
+    #     if current_user.propmts.include?(@propmt)
+    #         if @prompt&.destroy
+    #             render json: {message: "Successfully deleted prompt!"}
     #         else
     #             render json: {error: @post.errors.full_messages.to_sentence}
     #         end
