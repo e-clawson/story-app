c3a313e1ae36e4eb14cd287f961a5df17a9db49d
Elizabeth Clawson <emclawson1@gmail.com>
2022-05-24 12:11:39 -0500

    create story card

---
+++ client/src/components/Stories/StoryCard.js
@@ -0,0 +1,16 @@
+import "./Story.css"
+
+const StoryCard = ({story}) => {
+
+    return (
+        <div className= "story-card">
+            <h2>Title: {story.story_title}</h2>
+            {/* <h2>Author: </h2> currently has access to user id - how do I get it access to username? */}
+            {/* <h2>Prompt {story.prompt_id?}</h2> not sure how I should link it to  */ }
+            <h4>Body: {story.story_body}</h4>
+            <button>Favorite 💖</button>
+        </div>
+    )
+}
+
+export default StoryCard
