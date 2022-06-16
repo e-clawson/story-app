c27ae3c46b44402ccf7f0d3e3e89cb95e71c3ffd
Elizabeth Clawson <emclawson1@gmail.com>
2022-06-16 13:48:47 -0500

    update searchBAr

--- client/src/components/Prompts/SearchBar.js
+++ client/src/components/Prompts/SearchBar.js
@@ -1,16 +1,16 @@
 
 const SearchBar = () => (
-    <form action="/" method="get">
+    <form action={`http://localhost:4000/api/v1/search/`} method="get">
         <label htmlFor="header-search">
-            <span className="visually-hidden">Search blog posts</span>
+            <span>Search Prompts by Title </span>
         </label>
         <input
             type="text"
             id="header-search"
-            placeholder="Search blog posts"
-            name="s" 
+            placeholder="Search by Title"
+            name="prompt_title" 
         />
-        <button type="submit">Search</button>
+        <button type="submit">Search 🔎 </button>
     </form>
 );
 
