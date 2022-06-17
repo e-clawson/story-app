f2c184dbd86236d33df5cdd26c59af38bf8e480c
Elizabeth Clawson <emclawson1@gmail.com>
2022-06-16 19:40:31 -0500

    final commit

--- client/src/components/Prompts/SearchResultCard.js
+++ client/src/components/Prompts/SearchResultCard.js
@@ -1,10 +1,14 @@
 const SearchResultCard = ({result}) => {
 
     return (
+        <>
+        <h2>Search Results:</h2>
         <div className= "prompt-card">
             <h3>Prompt Title: {result.prompt_title}</h3>
             <h4>Prompt Body: {result.prompt_body}</h4>
         </div>
+        <hr/>
+        </>
       )
 }
 
